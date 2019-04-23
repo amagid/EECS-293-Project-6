1bf812fff71e19fa3e675eac6505e693bd21352a
Aaron Magid <ahm64@case.edu>
2019-04-23 07:28:35 -0400

    test(Connector): Completing code coverage

--- typed_parser/test/test_connector.py
+++ typed_parser/test/test_connector.py
@@ -1,6 +1,7 @@
 import pytest
 from typed_parser.connector import Connector
 from typed_parser.terminal_symbol import TerminalSymbol
+from typed_parser.abstract_token import AbstractToken
 
 def test_error_on_no_token_type():
     with pytest.raises(ValueError):
@@ -20,3 +21,20 @@ def test_connector_type_matches():
 
 def test_connector_str():
     assert str(Connector.build(TerminalSymbol.DIVIDE)) == '/'
+
+def test_connector_str_invalid_type():
+    conn = Connector.build(AbstractToken())
+    with pytest.raises(ValueError):
+        str(conn)
+
+def test_connector_equal():
+    conn = Connector.build(TerminalSymbol.PLUS)
+    conn2 = Connector.build(TerminalSymbol.PLUS)
+
+    assert conn == conn2
+
+def test_connector_not_equal():
+    conn = Connector.build(TerminalSymbol.PLUS)
+    conn2 = Connector.build(TerminalSymbol.MINUS)
+
+    assert conn != conn2
