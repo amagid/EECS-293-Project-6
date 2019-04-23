356176d3c0c93de5a8390197ad3b340d28a39aae
Aaron Magid <ahm64@case.edu>
2019-04-23 07:45:01 -0400

    test(Token): Completing code coverage

--- typed_parser/test/test_token.py
+++ typed_parser/test/test_token.py
@@ -4,3 +4,8 @@ from typed_parser.tokenclass import Token
 def test_token_is_abstract():
     with pytest.raises(TypeError):
         Token()
+
+def test_get_full_coverage_of_abstract_methods():
+    Token.get_type(None)
+    Token.matches(None, None)
+    assert True
