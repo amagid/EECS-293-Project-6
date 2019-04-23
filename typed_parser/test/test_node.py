7e122953c24f3a7170075b900cf8ab5da35a9ee1
Aaron Magid <ahm64@case.edu>
2019-04-23 07:35:41 -0400

    test(Node): Completing code coverage

--- typed_parser/test/test_node.py
+++ typed_parser/test/test_node.py
@@ -4,3 +4,9 @@ from typed_parser.node import Node
 def test_node_is_abstract():
     with pytest.raises(TypeError):
         Node()
+
+def test_get_node_coverage_even_though_it_is_abstract():
+    Node.to_list(None)
+    Node.get_children(None)
+    Node.is_fruitful(None)
+    Node.is_operator(None)
